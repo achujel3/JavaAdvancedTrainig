6224f235a57f47ff975701a33209a76f910748eb
achujelas123 <achujel3@gmail.com>
2017-03-08 12:00:23 +0200

    testing finally block

---
+++ src/com/lynda/tests/o602/testingFinallyBlock/MainFinallyBlock.java
@@ -0,0 +1,40 @@
+package com.lynda.tests.o602.testingFinallyBlock;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainFinallyBlock {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileReader = new FileReader("src/com/lynda/tests/o602/testingFinallyBlock/file/SomeText.txt");
+            bufferedReader = new BufferedReader(fileReader);
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Executing finally");
+            if(fileReader != null) {
+                fileReader.close();
+            }
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        }
+        System.out.println("Still alive");
+
+
+    }
+
+}
